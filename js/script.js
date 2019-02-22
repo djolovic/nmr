b63db4c1067f86ae06305bc8821ad79b4b6fca59
Aleksandar Djolovic <djole@omszone.com>
2019-02-22 09:49:57 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//modal image
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('rev');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+
+
+for(x=0;x<img.length;x++){
+    img[x].onclick = function(){
+        modalImg.src = this.src;
+        modal.style.display = "block";
+        captionText.innerHTML = this.alt;
+
+    }
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+
